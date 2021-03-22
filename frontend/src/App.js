6d1a3d12c55ebbf9c12f994f959b7d2ae9f75c3c
Maciej Celinski <maciej.celinski@ives-system.com>
2021-03-22 21:26:18 +0100

    Lecture 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import { Container } from "react-bootstrap";
+
 const App = () => {
 	return (
 		<>
-			<h1>Welcome to Pro Shop</h1>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to Pro Shop</h1>
+				</Container>
+			</main>
+			<Footer />
 		</>
 	);
 };
