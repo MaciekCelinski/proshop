40d298de7b7a914a7daa98fb2bf5f01a807d8c88
Maciej Celinski <maciej.celinski@ives-system.com>
2021-03-25 21:31:17 +0100

    HomeScreen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+import HomeScreen from "./screens/HomeScreen";
+
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -9,7 +11,7 @@ const App = () => {
 			<Header />
 			<main className="py-3">
 				<Container>
-					<h1>Welcome to Pro Shop</h1>
+					<HomeScreen />
 				</Container>
 			</main>
 			<Footer />
