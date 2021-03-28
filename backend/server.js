929109702d82606884a5d3a2814f44bf92e8a999
Maciej Celinski <maciej.celinski@ives-system.com>
2021-03-28 18:30:16 +0200

    Connected to database

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
 
+import connectDB from "./config/db.js";
+
 dotenv.config();
 const app = express();
 
+connectDB();
+
+// routes
 app.get("/", (req, res) => {
 	res.send("Api is running");
 });
