77d7f1bbff3d190534b7a8f963bd1b3c2522071e
Maciej Celinski <maciej.celinski@ives-system.com>
2021-04-05 11:44:55 +0200

    add controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,38 +1,18 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-// import model
-import Product from "../models/ProductModel.js";
+
+// controllers
+import {
+	getProducts,
+	getProductById,
+} from "../controllers/productController.js";
+
 // init router
 const router = express.Router();
 
-// GET all
-// @access public
-router.get(
-	"/",
-	asyncHandler(async (req, res) => {
-		const products = await Product.find({});
-		// res.status(401);
-		// throw Error("NOT AUTHORIZED");
-		res.json(products);
-	})
-);
+// router.get("/", getProducts);
+router.route("/", getProducts);
 
-// GET one
-// @access public
-router.get(
-	"/:id",
-	asyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id);
-
-		if (product) {
-			res.json(product);
-		} else {
-			res.status(404);
-			// now this goes to error middleware and is treated as err.message in errorHandler
-			throw new Error("Product not found");
-			// res.status(404).json({ message: "Product not found" });
-		}
-	})
-);
+// router.get("/:id", getProductById);
+router.route("/:id", getProductById);
 
 export default router;
